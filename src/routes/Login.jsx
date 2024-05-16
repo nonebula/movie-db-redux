0df48a9c811a0ec75e5f04d37bcb1cc7acb0fe67
Joe Higgs <higgs92@icloud.com>
2024-05-16 16:13:37 +0100

    login form & styling

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,7 +1,47 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useState } from "react";
+import styles from "../styles/Login.module.css";
 
 const Login = () => {
-  return <div></div>;
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate("/");
+    setName("");
+    setPassword("");
+  };
+
+  return (
+    <div className={styles.formWrapper}>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <div className={styles.inputWrapper}>
+          <label htmlFor="name">Username</label>
+          <input
+            type="text"
+            id="name"
+            required
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+        </div>
+        <div className={styles.inputWrapper}>
+          <label htmlFor="password">Password</label>
+          <input
+            type="text"
+            id="name"
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Sign In</button>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
